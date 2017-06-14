6305213aa6d1d6a0863e961441bebcb2cbb32c16
C0618C <weicong75@gmail.com>
2017-06-14 21:08:39 +0800

    job done

--- DB/sqlhelper.js
+++ DB/sqlhelper.js
@@ -18,6 +18,12 @@ class SqlParameter {
         }
 }
 
+class SqlItem {
+    constructor(sql, [...params]) {
+        this.sql = sql;
+        this.param = [...params];
+    }
+}
 
 class SQLHelper {
     constructor(cfg) {
@@ -38,6 +44,7 @@ class SQLHelper {
      * @param {*参数数组} parameters 
      */
     async GetDataTable(sqlString, parameters, callback) {
+        let rsl = null;
         this.myDB.then(db => {
             let rq = db.request();
             if (parameters.length > 0) {
@@ -46,19 +53,58 @@ class SQLHelper {
                 }
             }
             rq.query(sqlString).then(result => {
-                callback(result);
-            });
-        });
+                if (result.recordsets.length == 0) throw new Error("找不到记录：" + sqlString);
+                let datatable = result.recordsets[0];
+                callback(datatable);
+            }).catch(error => console.log(`查询失败：\nSQL:[${sqlString}]\n错误信息：${error}`));
+        }).catch(error => console.log("数据库连接失败：", error));
+
+        return rsl;
+    }
+
+    async ExecuteNonQuery([...sqlItem]) {
+        const transaction = new sql.Transaction(this.myDB);
+
+        transaction.begin(err => {
+            console.log("begin err:",err);
+
+            const request = new sql.Request(transaction)
+            request.query('insert into mytable (mycolumn) values (12345)', (err, result) => {
+                console.log("query err:",err);
+
+                transaction.commit(err => {
+                    console.log("commit err:",err);
+
+                    console.log("Transaction committed.")
+                })
+            })
+        })
     }
 }
 
 sql.on('error', err => {
-    // ... error handler 
     console.error(err)
 });
 
+/**
+ *  预定义数据库链接
+ */
+//Others数据库
 exports.OthersDB = () => {
     return new SQLHelper(dbcfg.othersdb);
+}
+//Crm数据库
+exports.CrmDB = () => {
+    console.error("CrmDB is unset !!!!");
+    return new SQLHelper(dbcfg.othersdb);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+function GetDbType(type, ...x) {
+    if (x.length == 0)
+        return sql[type];
+    return sql[type](...x);
 }
 
 /**
@@ -67,6 +113,7 @@ exports.OthersDB = () => {
  * @param {*数据类型，详情：https://www.npmjs.com/package/mssql#data-types} dbType 
  * @param {*值} value 
  */
-exports.MakeInParam = (paramName, dbType, value) => {
-    return new SqlParameter(paramName, sql.VarChar(36), value);
+exports.MakeInParam = (paramName, x, value) => {
+    let type = Array.isArray(x) ? GetDbType(...x) : GetDbType(x);
+    return new SqlParameter(paramName, type, value);
 }
--- sqltest.js
+++ sqltest.js
@@ -3,14 +3,13 @@ const odb = SQLHelper.OthersDB();
 
 (async function () {
     try {
-        await odb.GetDataTable("select top 1* from bpm_tl where id = @id", [SQLHelper.MakeInParam("id","","62F5433E-76C1-415F-9E5E-AB26812D3C84")], result => {
+        await odb.GetDataTable("select top 1* from bpm_tl where id = @id;select top 1* from BPM_DISA", [SQLHelper.MakeInParam("id",["VarChar",36],"62F5433E-76C1-415F-9E5E-AB26812D3C84")], result => {
             console.log(result);
-            console.warn("TT1");
         });
         await odb.GetDataTable("select top 1* from BPM_DISA", [], result => {
             console.log(result);
             console.warn("TT2");
-        })
+        });
     } catch (err) {
         console.log(err);
         // ... error checks 
